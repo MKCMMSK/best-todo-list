e03fe434a4f039eb10c99f37420ce4bb9b1c6adb
Shao <colinmaisk@gmail.com>
2019-11-16 21:18:48 +0000

    added users data and categories

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,13 +1,15 @@
 -- Users table seeds here (Example)
 INSERT INTO users (full_name, email, phone, img, pw, signup_date, permissions)
-VALUES ('Colin', 'colin@gmail.com', 789889123, 'url', 'asdfsadfsa', '04-11-2019', 'true');
+VALUES ('Colin', 'colin@gmail.com', 789889123, 'url', 'asdfsadfsa', '04-11-2019', 'true'),
+('Vanessa', 'Vanessa@gmail.com', 823943923, 'url2', 'poertujkdfg', '04-11-2019', 'true'),
+('Michelle', 'Michelle@gmail.com', 093458034, 'url3', 'cvbuewrbmm', '04-11-2019', 'true')
+;
 
+INSERT INTO categories (name)
+VALUES ('book'),
+('media'),
+('product'),
+('restaurant');
 
 -- id SERIAL PRIMARY KEY NOT NULL,
---   full_name VARCHAR (50) NOT NULL,
---   email VARCHAR(40) NOT NULL,
---   phone INTEGER,
---   img TEXT,
---   pw VARCHAR(25) NOT NULL,
---   signup_date DATE NOT NULL,
---   permissions BOOLEAN NOT NULL DEFAULT false
+--   name VARCHAR(20) NOT NULL
