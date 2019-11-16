02c675eaa87d133c40120d7266cf2053654a7cb3
Shao <colinmaisk@gmail.com>
2019-11-16 22:22:08 +0000

    added preferences for all users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -11,5 +11,26 @@ VALUES ('book'),
 ('product'),
 ('restaurant');
 
--- id SERIAL PRIMARY KEY NOT NULL,
---   name VARCHAR(20) NOT NULL
+INSERT INTO todo_items (cat_id,title, description, url, img)
+VALUES (4,'Blue water cafe', 'Seafood baby', 'BlueWater.com', 'BlueWater.com/pic'),
+(1,'War and peace', 'fighting and no fighting', 'W&P.com', 'W&P.com/pic'),
+(2,'Game of Thrones', 'fighting and drama', 'GoT.com', 'GoT.com/pic');
+
+INSERT INTO preferences (user_id,cat_id, pref_order, position)
+VALUES (1, 1, 4,4), /* colin preferences */
+(1, 2, 3,3),
+(1, 3, 2,2),
+(1, 4, 1,1),
+(2, 1, 2,2), /* Vanessa preferences */
+(2, 2, 1,1),
+(2, 3, 4,4),
+(2, 4, 3,3),
+(3, 1, 2,2),/* Michelle preferences */
+(3, 2, 3,3),
+(3, 3, 1,1),
+(3, 4, 4,4)
+;
+--  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   cat_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+--   pref_order INTEGER NOT NULL,
+--   position INTEGER
