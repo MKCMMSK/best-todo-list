acc456fecc6a913bb1c1beac641fa4ab65943d78
Shao <colinmaisk@gmail.com>
2019-11-16 21:30:08 +0000

    added values for todo_items

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS todo_item CASCADE;
+DROP TABLE IF EXISTS todo_items CASCADE;
 DROP TABLE IF EXISTS preferences CASCADE;
 DROP TABLE IF EXISTS to_do_user_specifics CASCADE;
 DROP TABLE IF EXISTS products CASCADE;
@@ -26,7 +26,7 @@ CREATE TABLE categories (
   name VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE todo_item (
+CREATE TABLE todo_items (
   id SERIAL PRIMARY KEY NOT NULL,
   cat_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   title VARCHAR(20) NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE preferences (
 
 CREATE TABLE to_do_user_specifics (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  item_id INTEGER REFERENCES todo_item(id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES todo_items(id) ON DELETE CASCADE,
   note text,
   archived boolean NOT NULL DEFAULT false,
   position INTEGER NOT NULL,
@@ -56,21 +56,21 @@ CREATE TABLE to_do_user_specifics (
 );
 
 CREATE TABLE products (
-  item_id INTEGER REFERENCES todo_item(id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES todo_items(id) ON DELETE CASCADE,
   brand VARCHAR(20),
   vendor VARCHAR(20),
   cost money NOT NULL
 );
 
 CREATE TABLE books (
-  item_id INTEGER REFERENCES todo_item(id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES todo_items(id) ON DELETE CASCADE,
   author VARCHAR(20),
   length INTEGER,
   genre VARCHAR(25)
 );
 
 CREATE TABLE movies_tv (
-  item_id INTEGER REFERENCES todo_item(id),
+  item_id INTEGER REFERENCES todo_items(id),
   director VARCHAR(20),
   length time,
   actors text,
@@ -78,7 +78,7 @@ CREATE TABLE movies_tv (
 );
 
 CREATE TABLE restaurants (
-  item_id INTEGER REFERENCES todo_item(id),
+  item_id INTEGER REFERENCES todo_items(id),
   location VARCHAR(75),
   CUISINE VARCHAR(25)
 );
