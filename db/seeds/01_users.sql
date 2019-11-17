0862f5c5ba51c47abb515e2905e9dfa3b6005b72
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-17 18:57:38 +0000

    add values for new table fields

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -11,13 +11,13 @@ VALUES ('book'),
 ('product'),
 ('restaurant');
 
-INSERT INTO todo_items (cat_id,title, description, url, img)
+INSERT INTO todo_items (category_id, title, description, url, img)
 VALUES (4,'Blue water cafe', 'Seafood baby', 'BlueWater.com', 'BlueWater.com/pic'),
 (1,'War and peace', 'fighting and no fighting', 'W&P.com', 'W&P.com/pic'),
 (2,'Game of Thrones', 'fighting and drama', 'GoT.com', 'GoT.com/pic'),
 (3, 'yeezes', 'freshes shoes in the whole damn game', 'kanye.com', 'kanye,con/yeezy');
 
-INSERT INTO preferences (user_id,cat_id, pref_order, position)
+INSERT INTO preferences (user_id, category_id, pref_order, position)
 VALUES (1, 1, 4,4), /* colin preferences */
 (1, 2, 3,3),
 (1, 3, 2,2),
@@ -33,21 +33,21 @@ VALUES (1, 1, 4,4), /* colin preferences */
 ;
 
 
-INSERT INTO to_do_user_specifics (user_id, item_id, note, archived, position, time_archived, rate, rating_comment)
+INSERT INTO to_do_user_specifics (user_id, todo_item_id, note, archived, position, time_archived, rate, rating_comment)
 VALUES (1, 3, 'cant wait to watch the terrible ending', 'false', '1', null, null, null),
 (2, 1, 'amazing seafood I hear', 'false', '1', null, null, null),
 (3, 2, 'amazing book I hear', 'true', '1', '2010-01-27', 5, 'GREAT BOOK');
 
-INSERT INTO products (item_id, brand, vendor, cost)
+INSERT INTO products (todo_item_id, brand, vendor, cost)
 VALUES (4, 'yeezy', 'adidas', 200.50);
 
-INSERT INTO books (item_id, author, length, genre)
-VALUES (2, 'writer of war and peace', 600, 'historic drama');
+INSERT INTO books (todo_item_id, author, publication_date, page_length, genre)
+VALUES (2, 'Leo Tolstoy, 1869, 1225, 'historic drama');
 
-INSERT INTO movies_tv (item_id, director, actors, genre)
+INSERT INTO movies_tv (todo_item_id, director, actors, genre)
 VALUES (3, 'P', 'Kid Herrington????', 'fantasy drama');
 
-INSERT INTO restaurants (item_id, location, cuisine)
+INSERT INTO restaurants (todo_item_id, location, cuisine)
 VALUES (1, 'Hamilton st. dt', 'Seafood');
 -- item_id INTEGER REFERENCES todo_items(id),
 --   location VARCHAR(75),
