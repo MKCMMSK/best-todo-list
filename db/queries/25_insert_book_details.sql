4dec0f3f33f9d2af16fd8ebb28f6030b3846ccc3
Michelle Siregar <michellesiregar@gmail.com>
2019-11-18 23:57:29 +0000

    edit statement to edit two tables in one query

--- db/queries/25_insert_book_details.sql
+++ db/queries/25_insert_book_details.sql
@@ -1,5 +1,10 @@
 -- insert new book details
 
+WITH new_todo AS (
+  INSERT INTO todo_items (category_id, title, description, url, img)
+  SELECT (1, 'Cat in the Hat', 'description', 'url', 'img')
+  WHERE NOT EXISTS (SELECT * FROM todo_items WHERE url = $4)
+  RETURNING *;
+)
 INSERT INTO books (todo_item_id, author, publication_date, page_length, genre)
-VALUES ($1, $2, $3, $4, $5)
-;
+VALUES ((SELECT id FROM new_tood), 'Dr. Seuss', '1234', '123', 'Childrens book')
