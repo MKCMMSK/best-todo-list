d9d8a3d3672678d3696a8e6a95393a70cb51df35
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-18 19:25:48 +0000

    query first 3 user items matching search form input

---
+++ db/queries/14_user_search_matching_items.sql
@@ -0,0 +1,11 @@
+--  All return first 3 records in a user's current list of todo items
+-- (archived and active) that match a user's search for a new item
+-- to add to their list
+
+SELECT *
+FROM users
+JOIN to_do_user_specifics ON users.id = user_id
+JOIN todo_items ON todo_items.id = to_do_user_specifics.todo_item_id
+WHERE users.id = 1
+AND todo_items.title ILIKE '%search text here%'
+LIMIT 3;
