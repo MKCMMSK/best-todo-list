5c37303119dfc3b87c75ee1f54bf6c69e7c805bd
Michelle Siregar <michellesiregar@gmail.com>
2019-11-18 22:25:24 +0000

    separate newtodo and newbook queries

--- db/database.js
+++ db/database.js
@@ -1,6 +1,11 @@
 const addBook = function(book) {
-  const queryString = `
+  const newToDo = `
+  INSERT INTO todo_items (category_id, title, description, url, img)
+  SELECT 2, 'new item', 'newdescription', 'newurl', 'newimg'
+  WHERE NOT EXISTS (SELECT * FROM todo_items WHERE url = 'http://www.noodleb')
+  ;`;
+  const newBook = `
   INSERT INTO books (todo_item_id, author, publication_date, page_length, genre)
   VALUES ($1, $2, $3, $4, $5)
-  ;`
+  ;`;
 }
