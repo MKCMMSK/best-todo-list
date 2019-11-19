822a051e22c0350b07222cd08dc5af06fffeced5
Michelle Siregar <michellesiregar@gmail.com>
2019-11-19 01:41:48 +0000

    add route helpers

---
+++ routes/route-helpers.js
@@ -0,0 +1,30 @@
+module.exports = (db) {
+  const addBook = function(book, userId) {
+    const newBook = `
+    WITH new_todo AS (
+      INSERT INTO todo_items (category_id, title, description, url, img)
+      SELECT $1, $2, $3, $4, $5
+      WHERE NOT EXISTS (SELECT * FROM todo_items WHERE url = $4)
+      RETURNING id
+    )
+    INSERT INTO books (todo_item_id, author, publication_date, page_length, genre)
+    SELECT (SELECT id FROM new_todo), $6, $7, $8, $9
+    WHERE EXISTS (SELECT * FROM new_todo)
+
+    INSERT INTO to_do_user_specifics (user_id, todo_item_id)
+    SELECT $10, (SELECT id FROM new_todo)
+    WHERE EXISTS (SELECT * FROM new_todo);`;
+
+    return pool
+    .query(newBook, [book.category_id, `${book.title}`, `${book.description}`, `${book.url}`, `${book.img}`, `${book.author}`, `${book.publication_date}`, book.page_length, `${book.genre}`, userId])
+    .then(res => res.rows)
+    .catch((err) => {
+      console.error(err);
+    });
+
+  }
+
+  return { addBook };
+
+};
+
