5e43d4a6f80eb1494e59aed70f2b3cf8afdfd78b
Michelle Siregar <michellesiregar@gmail.com>
2019-11-19 00:47:37 +0000

    add user specific statement to query

--- db/queries/25_insert_book_details.sql
+++ db/queries/25_insert_book_details.sql
@@ -8,4 +8,8 @@ WITH new_todo AS (
 )
 INSERT INTO books (todo_item_id, author, publication_date, page_length, genre)
 SELECT (SELECT id FROM new_todo), 'Dr. Seuss', '1234', '123', 'Childrens book'
+WHERE EXISTS (SELECT * FROM new_todo)
+
+INSERT INTO to_do_user_specifics (todo_item_id)
+SELECT (SELECT id FROM new_todo)
 WHERE EXISTS (SELECT * FROM new_todo);
