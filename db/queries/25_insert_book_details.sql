1d219198a5779dc02b99ca7a65cf1ad906d4d098
Michelle Siregar <michellesiregar@gmail.com>
2019-11-19 00:41:09 +0000

    fix query to only create if item doesnt exist

--- db/queries/25_insert_book_details.sql
+++ db/queries/25_insert_book_details.sql
@@ -2,9 +2,10 @@
 
 WITH new_todo AS (
   INSERT INTO todo_items (category_id, title, description, url, img)
-  SELECT (1, 'Cat in the Hat', 'description', 'url', 'img')
-  WHERE NOT EXISTS (SELECT * FROM todo_items WHERE url = $4)
-  RETURNING *;
+  SELECT 1, 'Cat in the Hat3', 'description', 'www.cathat3.com', 'img'
+  WHERE NOT EXISTS (SELECT * FROM todo_items WHERE url = 'www.cathat3.com')
+  RETURNING id
 )
 INSERT INTO books (todo_item_id, author, publication_date, page_length, genre)
-VALUES ((SELECT id FROM new_tood), 'Dr. Seuss', '1234', '123', 'Childrens book')
+SELECT (SELECT id FROM new_todo), 'Dr. Seuss', '1234', '123', 'Childrens book'
+WHERE EXISTS (SELECT * FROM new_todo);
