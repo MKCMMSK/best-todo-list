f73123aca9321346f0d9dab0bcab5d0647050ed7
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-19 01:43:11 +0000

    remove code from app.js that's going in app_helpers and client.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,4 @@
-const { api_helpers } = require("../../lib/util/api_helpers.js");
-
 $(document).ready(function () {
-
-  $.ajax({
-    method: "GET",
-    url: "/items" //gets product in position 0 aka only item yeezy
-  }).done((product) => {
-    renderList(product);
-  });
 
   // submit form with ajax
   $('#newToDo').submit((event) => {
@@ -24,34 +15,3 @@ $(document).ready(function () {
     console.log(media);
   });
 });
-
-const createListElement = function(object) { //creates simple list item need to implement overload for different categories
-  let item = `
-   <li>
-      <div class="collapsible-header">${object.title}</div>
-      <div class="collapsible-body">${object.description}</div>
-   </li>`;
-
-  return item;
-};
-
-const renderList = function(arr) { //prepends the database so that the top is the newest
-  for (let item of arr) {
-    switch (item.cat_id) {
-    case 1:
-      $(".to_read_list").prepend(createListElement(item));
-      break;
-    case 2:
-      $(".to_watch_list").prepend(createListElement(item));
-      break;
-    case 3:
-      $(".to_buy_list").prepend(createListElement(item));
-      break;
-    case 4:
-      $(".to_eat_list").prepend(createListElement(item));
-      break;
-    case 5:
-      $(".misc_list").prepend(createListElement(item));
-    }
-  }
-};
