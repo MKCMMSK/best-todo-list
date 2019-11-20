f685431c53e833fdaf3ff3d9c302cd13437d0a67
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-20 19:19:21 +0000

    set user in templateVars to session user id

--- server.js
+++ server.js
@@ -54,7 +54,9 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  const userID = req.session.user_id;
+  let templateVars = {user: userID};
+  res.render("index", templateVars);
 });
 
 app.listen(PORT, () => {
