3366a5be218a4c79952d5ee5ec9d6914e3fe5c1a
Shao <colinmaisk@gmail.com>
2019-11-22 04:12:50 +0000

    added edge cases for all api calls

--- routes/users.js
+++ routes/users.js
@@ -7,97 +7,45 @@
 
 const express = require('express');
 const router = express.Router();
-const { getAPIToDo } = require('../lib/util/api_helpers');
-const { addBook, getItems, addRestaurant, archiveItem } = require('../routes/helpers');
 
 module.exports = (helpers) => {
-
+  const { getAPIToDo, addToDB, changeCategory } = require('../lib/util/api_helpers')(helpers);
   router.get('/items', (req, res) => {
     let currentUser = null;
     if (req.session.user_id) {
       currentUser = req.session.user_id;
     };
-    //console.log(`current user: ${currentUser}`);
     helpers.getItems(currentUser)
       .then((products) => {
         res.send(products)
       })
   });
 
-  router.put('/', (req, res) => {
-    const userId = req.session.user_id;
-    const todo = req.body.archiveId;
-    if (!todo) {
-      res.send('400');
-    }
-    helpers.archiveItem(userId, todo)
-    .then(() => { res.send('deleted') })
-  });
-
-  router.get('/completed', (req, res) => {
-    let currentUser = null;
-    if (req.session.user_id) {
-      currentUser = req.session.user_id;
-    }
-    helpers.getCompleted(currentUser)
-      .then((products) => {
-        res.send(products)
-      })
-  });
-
-  router.put('/completed', (req, res) => {
-    const userId = req.session.user_id;
-    const todo = req.body.archiveId;
-    if (!todo) {
-      res.send('400');
-    }
-    helpers.unarchiveItem(userId, todo)
-      .then(() => { res.send('moved to to-do') })
-  });
-
-  router.post(('/login'), (req, res) => {
-    const email = req.body.email;
-    const userID = helpers.getUserId(email);
-    console.log(req.body);
-    if (userID) {
-      req.session.user_id = userID;
-      console.log("session cookie set to user id", req.session.user_id);
-      res.send('logged in');
-    };
+  router.get("/users/:id", (req, res) => {
+    const userID = req.params.id;
+    // set session cookie
+    req.session.user_id = userID;
+    res.redirect('/');
   });
 
   router.post('/', (req, res) => {
     const query = req.body.todo;
     const location = req.body.location;
-    getBook(query, (err, book) => {
-      helpers.addBook(book)
-        .then(() => { res.json(query) });
+
+    getAPIToDo(query, location, (search, b, item) => {
+      addToDB(search, item, (remainingObj) => {
+
+        res.json(remainingObj);
+
+      });
     })
 
-    getAPIToDo(query, location, (a, b, response) => {
-      console.log(response);
-      res.send(response);
-    })
-
-    // getRestaurant(query, location, (a, b, restaurant) => {
-    //   helpers.addRestaurant(restaurant)
-    //   .then(() => { res.json(query) });
-    // })
   });
 
   router.post('/logout', (req, res) => {
     req.session.user_id = null;
     res.redirect('/');
   });
-
-  router.post('/register', (req, res) => {
-    let userId = null;
-    const user = req.body;
-    helpers.register(user)
-    .then((user) => userId = helpers.getUserId(user.email))
-    .then((userId) => req.session.user_id = userId)
-    .then(() => res.send('created'))
-  })
 
   return router;
 };
